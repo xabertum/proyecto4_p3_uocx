c3a2ee44f77dc7c378ff9a84485b7c0b6e7605f1
xabertum <mohatar@gmail.com>
2019-04-23 11:45:46 +0200

    adaptation changes

--- vista/index.php
+++ vista/index.php
@@ -2,13 +2,6 @@
 include '../datos/conexion.php';
 
 $cnx = Conexion::conectar();
-$query = "SELECT id, titulo, subtitulo FROM noticias";
-$query_publicadas = "SELECT id, autor, titulo, fecha from noticias_publicadas";
-
-$resultado = $cnx->query($query);
-$resultado_publicadas = $cnx->query($query_publicadas);
-
-$noticias = $resultado->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
@@ -49,39 +42,7 @@ $noticias = $resultado->fetchAll(PDO::FETCH_ASSOC);
 						href="sign-in.html"> <img class="icon-position"
 							src="img/ic_account_circle_white_48dp_1x.png" height="24px"
 							alt="Sign-In Icon"> <span id="sign-position">Sign-In</span>
-					</a></li>
-
-					<li class="nav-item active li-wrapper li-color-1"><a
-						class="nav-link" href=""> <span>Home</span>
-					</a></li>
-
-					<li class="nav-item active li-wrapper"><a class="nav-link" href="">
-							<span>News</span>
-					</a></li>
-
-					<li class="nav-item active li-wrapper"><a class="nav-link" href="">
-							<span>Sports</span>
-					</a></li>
-
-					<li class="nav-item active li-wrapper"><a class="nav-link" href="">
-							<span>Weather</span>
-					</a></li>
-
-					<li class="nav-item active li-wrapper"><a class="nav-link" href="">
-							<span>Shop</span>
-					</a></li>
-
-					<li class="nav-item active li-wrapper"><a class="nav-link" href="">
-							<span>Earth</span>
-					</a></li>
-
-					<li class="nav-item active li-wrapper"><a class="nav-link" href="">
-							<span>Travel</span>
-					</a></li>
-
-					<li class="nav-item active li-wrapper"><a class="nav-link" href="">
-							<span>Capital</span>
-					</a></li>
+					</a></li>					
 				</ul>
 			</div>
 		</div>
@@ -99,67 +60,9 @@ $noticias = $resultado->fetchAll(PDO::FETCH_ASSOC);
 			<div class="row">
 				<div class="col-sm-6">
 					<div class="custom-container">
-						<a href="news-index.php?nid=<?php echo $noticias[0]['id']; ?>">
-							<img class="img-fluid" src="img/pexels-photo-92866.jpeg"
-								alt="latest news 1">
-							<div class="text-overlay">
-								<h2><?php echo $noticias[0]['titulo']; ?></h2>
-								<p><?php echo $noticias[0]['subtitulo']; ?></p>
-								<img class="barra-news" src="img/barraNewsRoja.png" alt="">
-								<p>LATIN AMERICA & CARIBBEAN</p>
-							</div>
-						</a>
+						
 					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="custom-container">
-						<a href="news-index.php?nid=<?php echo $noticias[1]['id']; ?>">
-							<img class="img-fluid"
-								src="img/building-vintage-bike-monument.jpg" alt="latest news 2">
-							<div class="text-overlay">
-								<h5><?php echo $noticias[1]['titulo'] ?></h5>
-								<img class="barra-news" src="img/barraNewsRoja.png" alt="">
-								<p>UK</p>
-							</div>
-						</a>
-					</div>
-
-					<div class="custom-container">
-						<a href="news-index.php?nid=<?php echo $noticias[2]['id']; ?>">
-							<img class="latest-news-padding img-fluid"
-								src="img/pexels-photo-396143.jpeg" alt="latest news 2">
-							<div class="text-overlay">
-								<h4><?php echo $noticias[2]['titulo'] ?></h4>
-								<img class="barra-news" src="img/barraNewsAzul.png" alt="">
-								<p>ASIA</p>
-							</div>
-						</a>
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="custom-container">
-						<a href="news-index.php?nid=<?php echo $noticias[4]['id']; ?>">
-							<img class="img-fluid" src="img/pexels-photo-808908.jpeg"
-								alt="latest news 2">
-							<div class="text-overlay">
-								<h4><?php echo $noticias[4]['titulo'] ?></h4>
-								<img class="barra-news" src="img/barraNewsRoja.png" alt="">
-								<p>EUROPE</p>
-							</div>
-						</a>
-					</div>
-					<div class="custom-container">
-						<a href="news-index.php?nid=<?php echo $noticias[3]['id']; ?>">
-							<img class="latest-news-padding img-fluid"
-								src="img/pexels-photo-10467.jpeg" alt="latest news 2">
-							<div class="text-overlay">
-								<h4><?php echo $noticias[3]['titulo'] ?></h4>
-								<img class="barra-news" src="img/barraNewsVerde.png" alt="">
-								<p>EARTH</p>
-							</div>
-						</a>
-					</div>
-				</div>
+				</div>				
 			</div>
 		</div>
 	</div>
@@ -175,158 +78,24 @@ $noticias = $resultado->fetchAll(PDO::FETCH_ASSOC);
 	<div class="container latest-news">
 		<div class="row">
 			<div class="col-3">
-				<a href="news-index.php?nid=<?php echo $noticias[5]['id']; ?>">
-					<img class="img-fluid news-img-padding" src="img/pexels-photo-169677.jpeg" alt="">
-					<h3><?php echo $noticias[5]['titulo'] ?></h3>
-				</a>
-					<p><?php echo $noticias[5]['subtitulo'] ?></p>
-					<img class="barra-news" src="img/barraNewsRoja.png" alt="">
-
-				<p>EUROPE</p>
+				
 
 			</div>
 
 			<div class="col-3">
-				<a href="news-index.php?nid=<?php echo $noticias[6]['id']; ?>">
-					<img class="img-fluid news-img-padding" src="img/pexels-photo-315191.jpeg" alt="">
-					<h3><?php echo $noticias[6]['titulo'] ?></h3>
-				</a>
-					<p><?php echo $noticias[6]['subtitulo'] ?></p>
-					<img class="barra-news" src="img/barraNewsRoja.png" alt="">
-
-				<p>EUROPE</p>
+				
 			</div>
 
 			<div class="col-3">
-				<a href="news-index.php?nid=<?php echo $noticias[8]['id']; ?>">
-					<img class="img-fluid news-img-padding" src="img/pexels-photo-373965.jpeg" alt="">
-					<h3><?php echo $noticias[8]['titulo'] ?></h3>
-				</a>
-					<p><?php echo $noticias[8]['subtitulo'] ?></p>
-					<img class="barra-news" src="img/barraNewsRoja.png" alt="">
-
-				<p>EUROPE</p>
+				
 			</div>
 
 			<div class="col-3">
-				<a href="news-index.php?nid=<?php echo $noticias[7]['id']; ?>">
-					<img class="img-fluid news-img-padding" src="img/pexels-photo-459694.jpeg" alt="">
-					<h3><?php echo $noticias[7]['titulo'] ?></h3>
-				</a>
-					<p><?php echo $noticias[7]['subtitulo'] ?></p>
-					<img class="barra-news" src="img/barraNewsRoja.png" alt="">
-
-				<p>EUROPE</p>
+				
 			</div>
 		</div>
 	</div>
 
-	<!-- OTHER NEWS -->
-	<div class="container-fluid latest-news latest-bg">
-		<div class="container">
-			<div class="row news-img-padding">
-				<div class="col-6">
-					<img class="barra-news" src="img/barraNewsAmarillo.png" height="33"
-						alt="">
-					<h2>Music</h2>
-				</div>
-
-				<div class="col-6">
-					<img class="barra-news" src="img/barraNewsAmarillo.png" height="33"
-						alt="">
-					<h2>Fashion</h2>
-				</div>
-			</div>
-		</div>
-
-		<div class="container ">
-			<div class="row">
-				<div class="col-6">
-				<a href="news-index.php?nid=<?php echo $noticias[9]['id']; ?>">
-					<img class="img-fluid news-img-padding" src="img/pexels-photo-704623.jpeg" alt="">
-					<h4 class="color-black"><?php echo $noticias[9]['titulo'] ?></h4>
-				</a>
-					<p><?php echo $noticias[9]['subtitulo'] ?></p>
-				</div>
-
-				<div class="col-3">
-				<a href="news-index.php?nid=<?php echo $noticias[10]['id']; ?>">
-					<img class="img-fluid moda" height="200" src="img/pexels-photo-715821.jpeg" alt="">
-				</a>
-				<a href="news-index.php?nid=<?php echo $noticias[11]['id']; ?>">
-					<img class="img-fluid moda" src="img/pexels-photo-715836.jpeg" alt="">
-				</a>
-				</div>
-
-				<div class="col-3">
-					<div class="moda">
-						<a href="news-index.php?nid=<?php echo $noticias[10]['id']; ?>">
-							<h4><?php echo $noticias[10]['titulo'] ?></h4>
-						</a>
-						<p><?php echo $noticias[10]['subtitulo'] ?></p>
-					</div>
-
-					<div class="moda-txt">
-						<a href="news-index.php?nid=<?php echo $noticias[11]['id']; ?>">
-							<h4><?php echo $noticias[11]['titulo'] ?></h4>
-						</a>
-						<p><?php echo $noticias[11]['subtitulo'] ?></p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<!-- LATEST NEWS PUBLISHED -->
-		<div class="container latest-news">
-			<div class="row">
-				<div class ="col-12">
-					<div>
-						<img class="barra-news" src="img/barraNewsAmarillo.png" height="33"
-							alt="">
-						<h2>Latest News Published</h2>
-					</div>
-
-					<table class="table table-hover table-striped table-dark">
-					<thead>
-						<tr>
-						<th scope="col">#</th>
-						<th scope="col">Autor</th>
-						<th scope="col">Titulo</th>
-						<th scope="col">Fecha</th>
-						</tr>
-					</thead>
-					<tbody>
-					<?php
-					
-					while ($noticias_publicadas = $resultado_publicadas->fetch(PDO::FETCH_ASSOC)) {
-												
-						echo '<tr>';
-						echo '<td>';
-						echo '<a href="latest-news/news-index-latest.php?nid='.$noticias_publicadas['id'].'">';
-						echo $noticias_publicadas['id'];						
-						echo '</td>';
-						echo '<td>';
-						echo $noticias_publicadas['autor'];
-						echo '</td>';
-						echo '<td>';
-						echo $noticias_publicadas['titulo'];
-						echo '</td>';
-						echo '<td>';
-						echo $noticias_publicadas['fecha'];
-						echo '</td>';
-						echo '</a>';
-						echo '</tr>';
-												
-					}
-
-					?>
-					</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-	</div>
 
 	<!-- FOOTER -->
 
