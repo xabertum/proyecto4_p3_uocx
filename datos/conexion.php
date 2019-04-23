d7107301b443e7106084b719a0fe2a5766045fd5
xabertum <mohatar@gmail.com>
2019-04-23 10:12:33 +0200

    Initial commit

--- datos/conexion.php
+++ datos/conexion.php
@@ -11,7 +11,7 @@ class Conexion {
         
         try {
            
-            $pdo = new PDO ('mysql:host=localhost;dbname=phplogin', 'root', '');
+            $pdo = new PDO ('mysql:host=localhost;dbname=phplogin', 'root', 'zanfona');
     
             return $pdo;
             
